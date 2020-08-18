3002c3d99858de293e821b5eadad93e25ac07f37
Muhammad Talha <talhamalik4147@gmail.com>
2020-08-18 20:44:36 +0500

    Aug 18, 2020 8:44

--- display.php
+++ display.php
@@ -4,5 +4,13 @@ include("connection.php");
 $readQuery = "SELECT * FROM client";
 
 $readData =  mysqli_query($conn, $readQuery);
+if ($readData) {
+    echo "Data Read";
+} else {
+    echo "Data Not Read";
+}
 
 ?>
+<script>
+    console.log("YES");
+</script>
